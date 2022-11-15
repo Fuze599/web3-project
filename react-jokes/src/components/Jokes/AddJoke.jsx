f9c75c3a4f6b44e477c60f015d99a39863468eb8
laurent vdm <laurent.vandermeersch@student.vinci.be>
2022-11-15 19:13:06 +0100

    addJoke complete

--- react-jokes/src/components/Jokes/AddJoke.jsx
+++ react-jokes/src/components/Jokes/AddJoke.jsx
@@ -1,5 +1,57 @@
+import { useState } from "react";
+import { useContext } from "react"
+import { Context as jokeContext } from "../../contexts/JokesContext"
+
 const AddJoke = () => {
-    return <p>ADDJOKE</p>
+    
+    const {createJoke} = useContext(jokeContext)    //mettre le bon context 
+    const [jokeContent, setJokeContent] = useState('')
+    const [jokeCategory, setJokeCategory] = useState('')
+
+    const handleContentChange = (event) => {
+        setJokeContent(event.target.value)
+    }
+
+    const handleCategoryChange = (event) =>{
+        setJokeCategory(event.target.value)
+    }
+
+    const addJoke = (event) => {
+      event.preventDefault()
+      const joke = {
+         "content":jokeContent,
+         "category":jokeCategory,
+         "date":Date.now()
+      }
+      createJoke(joke)
+      setJokeContent('')
+      setJokeCategory('')
+    }
+    
+    const options = [
+        {
+          label: "Second degre",    //site
+          value: "second degre",
+        },
+        {
+          label: "Autres",
+          value: "others",
+        },
+      ];
+
+    return(
+    <div>
+      <form onSubmit={addJoke}>
+        <input value = {jokeContent} onChange={handleContentChange}/>
+        <select value= {jokeCategory} onChange={handleCategoryChange}>
+            {options.map((option) => (
+              <option value={option.value}>{option.label}</option>
+            ))}
+          </select>
+        <button type="submit">Add Joke</button>
+      </form>
+    </div>
+    )
 }
 
 export default AddJoke
