5deb4b7af82380b5a78c7e1d5191bfa1477010e9
François Bardijn <francois.bardijn@hotmail.com>
2022-11-18 11:16:08 +0100

    jokes details

--- react-jokes/src/components/App/App.js
+++ react-jokes/src/components/App/App.js
@@ -3,20 +3,18 @@ import Home from "components/Home/Home"
 import AddJoke from "components/Jokes/AddJoke"
 import ListJokes from "components/Jokes/ListJokes"
 import JokeDetail from "components/Jokes/JokeDetail"
-import { Routes, Route, Link, useMatch } from "react-router-dom"
-import { useContext } from "react"
-import { Context as JokesContext } from '../../contexts/JokesContext'
+import { Routes, Route, Link } from "react-router-dom"
+// import { useContext } from "react"
+// import { Context as JokesContext } from '../../contexts/JokesContext'
 
 const App = () => {
 
-  const { jokes } = useContext(JokesContext)
-
-  const jokeById = (id) => jokes.find(a => a.id === id)
-  
-  const match = useMatch('/jokes/:id')
-  const joke = match
-    ? jokeById(Number(match.params.id))
-    : null
+  // const { jokes } = useContext(JokesContext)
+  // const match = useMatch('/jokes/:id')
+  //   const jokeById = (id) => jokes.find(a => a.id === id)
+  //   const joke = match
+  //     ? jokeById(Number(match.params.id))
+  //     : null
 
   return (
     <div>
@@ -32,7 +30,7 @@ const App = () => {
         <Route path='/jokes' element={<ListJokes />} />
         <Route path='/create' element={<AddJoke />} />
         <Route path='/about' element={<About />} />
-        <Route path='/jokes/:id' element={<JokeDetail joke={joke} />} />
+        <Route path='/jokes/:id' element={<JokeDetail />} />
       </Routes>
     </div>
   )
--- react-jokes/src/components/Jokes/JokeDetail.jsx
+++ react-jokes/src/components/Jokes/JokeDetail.jsx
@@ -1,7 +1,18 @@
-const JokeDetail = (joke) => {
+import { useContext, useEffect, useState } from "react"
+import { Context as JokesContext } from '../../contexts/JokesContext'
+import {useParams} from "react-router-dom";
 
-    console.log(joke.content);
+const JokeDetail = () => {
+    const [joke, setJoke] = useState({})
+    const { getJoke } = useContext(JokesContext)
 
+    const jokeId = useParams().id
+    useEffect(() => {
+        const loadJoke = async () => {
+            setJoke(await getJoke(jokeId))
+        }
+        loadJoke()
+      }, [getJoke, jokeId])
     return (
         <>
             <div>{joke.content}</div>
