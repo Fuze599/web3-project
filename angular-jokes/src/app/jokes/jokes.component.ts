8c75b5bfa62b45870824de3da4012e9d81518e14
NicolasDed <nicolas.dedoyard@student.vinci.be>
2022-11-22 18:01:29 +0100

    delete not used code

--- angular-jokes/src/app/jokes/jokes.component.ts
+++ angular-jokes/src/app/jokes/jokes.component.ts
@@ -12,7 +12,6 @@ export class JokesComponent implements OnInit{
   jokes: Joke[] = [];
   filteredJokes: Joke[] = [];
   categories: string[] = [];
-  category: string = "";
 
   constructor(private jokeService: JokeService) { }
 
