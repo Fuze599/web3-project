a22af39771c642677e090342a2b5915ce7bf9281
François Bardijn <francois.bardijn@hotmail.com>
2022-11-22 10:01:53 +0100

    zizi

--- react-jokes/src/components/Jokes/ListJokes.jsx
+++ react-jokes/src/components/Jokes/ListJokes.jsx
@@ -1,10 +1,13 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { Context as JokesContext } from '../../contexts/JokesContext'
 import { useNavigate } from "react-router-dom"
 const ListJokes = () => {
 
   const { jokes } = useContext(JokesContext)
   const navigate = useNavigate()
+
+  const [search, setSearch] = useState('')
+  const [searchCategory, setSearchCategory] = useState('')
 
   const jokesStyle = {
     margin: "20px",
@@ -14,9 +17,27 @@ const ListJokes = () => {
     "border-radius": "4px"
   }
 
+  const options = [
+    {
+      label: "Second degre",    //site
+      value: "second degre",
+    },
+    {
+      label: "Autres",
+      value: "others",
+    },
+  ];
+
   return (
     <>
       <h1>All jokes</h1>
+      <input type="text"/>
+      <br/>
+      <select value= {searchCategory}>
+        {options.map((option) => (
+          <option value={option.value}>{option.label}</option>
+        ))}
+      </select>
       {jokes.map((joke, index) => (
         <div onClick={() => navigate(`/jokes/${joke.id}`)} key={joke.id} style={jokesStyle}>
           <p>{index} - {joke.content}</p>
